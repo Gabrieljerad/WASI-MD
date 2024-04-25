b0ee5dcd06e9779ba56287559dab921b1b3316c0
Gabrieljerad <gabrieljerad448@gmail.com>
2024-04-25 15:59:49 +0300

    config.js

--- config.js
+++ config.js
@@ -92,7 +92,7 @@ module.exports = {
   aitts_Voice_Id : process.env.AITTS_ID || "01",
   ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
   WORKTYPE: process.env.WORKTYPE||process.env.MODE || "private",
-  LANG: process.env.THEME ? process.env.THEME.toUpperCase() : "NEZUKO",
+  LANG: process.env.THEME ? process.env.THEME.toUpperCase() : "WASI",
 
 
 
